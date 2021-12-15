d81cca4b94196b3ddc07df8dc0ac5abe2f1321bc
jihun <limjihun204@naver.com>
2021-12-16 08:21:21 +0900

    Init2

---
+++ nftviewer/src/main/java/com/nouvelle/nftviewer/controller/NftViewController.java
@@ -0,0 +1,14 @@
+package com.nouvelle.nftviewer.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+@RequestMapping(value = "/user")    // 자동 import -> Alt Shift O
+public class NftViewController {
+
+    
+    
+}
